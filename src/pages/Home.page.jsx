25f67a3d49bad00e34ec4c30877fbe84f8c5671e
ratedboy <dvloper.ir@gmail.com>
2023-10-05 22:26:36 +0330

    New V

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SpinnerComponent from '../components/UI/Spinner.component';
 
 const HomePage = () => {
     return (
         <div>
             <h5>HomePage</h5>
+            <SpinnerComponent></SpinnerComponent>
         </div>
     );
 }
