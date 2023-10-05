09ea6b89ba48efb8a66fb6e169c2800f9e56ccf5
ratedboy <dvloper.ir@gmail.com>
2023-10-05 20:55:15 +0330

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import FooterComponent from "./components/UI/Footer.component";
+import NavbarComponent from "./components/UI/Navbar.component";
+
 function App() {
   return (
     <div>
-      <h3>Rated</h3>
+      {/* Navbar */}
+      <NavbarComponent />
+
+      {/* Wrapper */}
+      <div className="wrapper">
+          <h2>Wrapper</h2>
+      </div>
+
+      {/* Footer */}
+      <FooterComponent />
+
     </div>
   );
 }
-
 export default App;
